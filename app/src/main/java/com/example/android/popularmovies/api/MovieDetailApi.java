c8bbffa556e1d04c53874aaa21a7e3dfa39a05ee
Jose Luis Sanchez <joseluis.sanchez84@gmail.com>
2017-10-03 21:56:29 -0500

    Add methods fetchMovieTrailers and fetchMovieRewiews to query API MovieDB.

--- app/src/main/java/com/example/android/popularmovies/api/MovieDetailApi.java
+++ app/src/main/java/com/example/android/popularmovies/api/MovieDetailApi.java
@@ -1,9 +1,12 @@
 package com.example.android.popularmovies.api;
 
 import com.example.android.popularmovies.data.MovieDetail;
+import com.example.android.popularmovies.data.MovieReview;
+import com.example.android.popularmovies.data.MovieTrailer;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 /**
  * Interface to get list of movies.
@@ -17,4 +20,10 @@ public interface MovieDetailApi {
 
     @GET("movie/top_rated")
     Call<MovieDetail.Response> discoverMoviesTopRated();
+
+    @GET("movie/{id}/videos")
+    Call<MovieTrailer.Response> fetchMovieTrailers(@Path("id") String id);
+
+    @GET("movie/{id}/reviews")
+    Call<MovieReview.Response> fetchMovieReviews(@Path("id") String id);
 }
