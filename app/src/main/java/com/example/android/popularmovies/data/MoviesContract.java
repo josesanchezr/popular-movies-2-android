bbc9a6e19be25a682586b91b1335abf1dd6d2f78
Jose Luis Sanchez <joseluis.sanchez84@gmail.com>
2017-10-03 21:49:41 -0500

    Create MoviesContract by to save info of favorite movies.

---
+++ app/src/main/java/com/example/android/popularmovies/data/MoviesContract.java
@@ -0,0 +1,44 @@
+package com.example.android.popularmovies.data;
+
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * Created by joseluis on 2/10/2017.
+ */
+
+public class MoviesContract {
+
+    public static final String CONTENT_AUTHORITY = "com.example.android.popularmovies";
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final class MoviesEntry implements BaseColumns {
+        // table name
+        public static final String TABLE_MOVIES = "movie";
+        // columns
+        public static final String _ID = "_id";
+        public static final String COLUMN_ORIGINAL_TITLE = "original_title";
+        public static final String COLUMN_POSTER_PATH = "poster_path";
+        public static final String COLUMN_OVERVIEW = "overview";
+        public static final String COLUMN_VOTE_AVERAGE = "vote_average";
+        public static final String COLUMN_RELEASE_DATE = "release_date";
+
+        // create content uri
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon()
+                .appendPath(TABLE_MOVIES).build();
+        // create cursor of base type directory for multiple entries
+        public static final String CONTENT_DIR_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE +
+                "/" + CONTENT_AUTHORITY + "/" + TABLE_MOVIES;
+        // create cursor of base type item for single entry
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + TABLE_MOVIES;
+
+        // for building URIs on insertion
+        public static Uri buildFlavorsUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+    }
+}
